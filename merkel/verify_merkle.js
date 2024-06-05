3519afa7961f1429821b6b3997cb1ee42d1b6302
Cewei Fu <fucewei@jancsitech.net>
2024-06-05 16:06:30 +0800

    update: whiteList;

--- merkel/verify_merkle.js
+++ merkel/verify_merkle.js
@@ -4,36 +4,37 @@ const fs = require('fs');
 const { log } = require('console');
 
 
-// const jsonString = fs.readFileSync('./whiteList.json', 'utf8');
-// const WhiteList = JSON.parse(jsonString);
-// const leaves = WhiteList.map((x) => keccak256(x));
-// const merkleTree = new MerkleTree(leaves, keccak256, { sortPairs: true });
-// const rootHash = '0x' + merkleTree.getRoot().toString('hex');
-// const testAddress = '0x58841cb93a81b3aa2b1de2bebd9bd9fb6490263f';  //example
-// const leaf = keccak256(testAddress);
-// const proof = merkleTree.getHexProof(leaf);
-// const isWhiteList = merkleTree.verify(proof, leaf, rootHash);
+const jsonString = fs.readFileSync('./whiteList.json', 'utf8');
+const WhiteList = JSON.parse(jsonString);
+const leaves = WhiteList.map((x) => keccak256(x));
+const merkleTree = new MerkleTree(leaves, keccak256, { sortPairs: true });
+const rootHash = '0x' + merkleTree.getRoot().toString('hex');
+const testAddress = '0x58841cb93a81b3aa2b1de2bebd9bd9fb6490263f';  //example
+const leaf = keccak256(testAddress);
+const proof = merkleTree.getHexProof(leaf);
+const isWhiteList = merkleTree.verify(proof, leaf, rootHash);
 
-// console.log('rootHash:: ', rootHash);
-// console.log('proof:: ', proof);  // Contract input, double quotes instead of single quotes
-// console.log('verify:: ', isWhiteList);
+console.log('rootHash:: ', rootHash);
+console.log('proof:: ', proof);  // Contract input, double quotes instead of single quotes
+console.log('verify:: ', isWhiteList);
 
-// rootHash::  0x9975b4e80ee80c53b7d3ad7dea09f60b11c14c4813f9d2d71a19c33c64c42b84
+
+// rootHash::  0x1616fe05a5e33494f9daa37158d53e5a30d62b9d0e3e8bc022472f1cee3cca98
 // proof::  [
-//     '0x7752443926179ab91a043ecc86eaf2cb14ca7a648a61e77503ecc4823a694686',
-//     '0x80494ef4bf243f44d15738160cdaf301ccd3a91b9c0a133c1e220d79314689db',
-//     '0xdf8e830b5084fb4262082317003e6029dba2a689237a664b31cda8030e40923c',
-//     '0x110d5bf14548f7bbb3e7049911c9e37eeda57dd3d47dc4a01f564e17dd97eceb',
-//     '0x40534def59aa4e361f69e7cd473c14f84a1afc69796904286f1a3dec238b0a6f',
-//     '0x1c16a3ebde95080cab7610ddf4971754e54561efe5ff77615f31dadc45aaae2b',
-//     '0x8827aeb3c8663af58bf028cc1655623507b046bb3e20dea6bdccb309097f2fa2',
-//     '0x5e2c77a884288caf84d63530a31b6418126313ce9687c4f04af1c995d35f8dd9',
-//     '0x82a748e72e44a168b7f777953b566f283417b0005a3cdee7aaf587435910f27e',
+//     '0xd02dd8df66b7981f4186e837b5601f692acbeb7460d6facef1e2997bc653dd29',
+//     '0x4e591a967a5d6d4831ec0a4750c0b3a62a6acab8074c2ab11b0c639115b68b0e',
+//     '0x41dadca109deeb98fd1e005e85f053457c577bdd5beedcaf66e78871bba8f939',
+//     '0xb292bb2a24fc8936c9b50e194a1f00a68012385992e07f9c6b220d309c0e30a6',
+//     '0xa757bf3fef850f5b57086faadde26059af5d1b9bc6907214df836f10652ee68a',
+//     '0xc9ab8398dd143ac88fcd849df3785ab9174c93bef42fbe8e946225ab4b84e5e3',
+//     '0xe49dd2777c658aeccec94b8a21f485d942ece3488926fe93ae997aca2a736533',
+//     '0x59d0ce7e3fa1a03ae152ae3fa6d96f4fa8d85ab34f5f76a1718237761e135049',
+//     '0xd71c8ce25befa691d6e96012300760ced54c530c0648dc45c704ceef9767a991',
 //     '0xfbce61c8c7d129d5521b85d9be20813e9a45b909f227ca7fbc1abcdd4827d291',
-//     '0x4e0c29d5e80e6342c82c488d4babc86589231993427de89f1395ac1bd06fc960'
+//     '0xe06174cc2dadc776d95e45c71c215523cf0e06e3e6b804081a2938a2815f0086',
+//     '0x3b0409fae75fe3205533ea83755bc87df326d0a73ad2630a73d191427aca078a'
 // ]
 // verify::  true
-
 
 // Read whitelist addresses and create Merkle trees
 function createMerkleTree() {
