044f17c629ac1109bd28144c60fc7d5cadb59a85
CEWEI FU <1457310354@qq.com>
2024-07-07 16:09:36 +0800

    add: batch joinIdo;

--- ghostDB/cmd/gui/main.go
+++ ghostDB/cmd/gui/main.go
@@ -6,6 +6,7 @@ import (
 	"ghostDB/cmd/batchTransfer"
 	"ghostDB/cmd/getWalletPri"
 	"ghostDB/cmd/integrate"
+	"ghostDB/cmd/whiteListJoinIdo"
 	"ghostDB/util"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/patrickmn/go-cache"
@@ -36,6 +37,7 @@ var rootCmd = &cobra.Command{
 			fmt.Println("4. 设置主钱包(目前的主钱包是:" + mainWallet + ")")
 			fmt.Println("5. 设置网络节点(目前的网络节点是:" + netNode + ")")
 			fmt.Println("6. 查看钱包地址私钥")
+			fmt.Println("7. 批量  Join Ido")
 			fmt.Println("-------- Ctrl + c 退出程序 --------")
 
 			var choice int
@@ -63,7 +65,7 @@ var rootCmd = &cobra.Command{
 				fmt.Scanln(&distributeFeesArgs)
 				distributeFeesCmd.Run(cmd, []string{preRange, afterRange, distributeFeesArgs})
 			case 3:
-				fmt.Println("请输入批量 mint 合约函数地址是: ")
+				fmt.Println("请输入批量 mint 合约地址是: ")
 				var mintContractAddress string
 				fmt.Scanln(&mintContractAddress)
 				fmt.Println("请输入该合约的abi文件的名称是(.json结尾文件): ")
@@ -105,6 +107,32 @@ var rootCmd = &cobra.Command{
 				var afterRange string
 				fmt.Scanln(&afterRange)
 				getWalletKeyCmd.Run(cmd, []string{preRange, afterRange})
+
+			case 7:
+				fmt.Println("请输入批量 joinIdo 合约地址是: ")
+				var mintContractAddress string
+				fmt.Scanln(&mintContractAddress)
+				fmt.Println("请输入该合约的abi文件的名称是(.json结尾文件): ")
+				var abiFileName string
+				fmt.Scanln(&abiFileName)
+				fmt.Println("请输入批量joinIdo的钱包地址的前置范围是:")
+				var preRange string
+				fmt.Scanln(&preRange)
+				fmt.Println("请输入批量joinIdo的钱包地址的后置范围是:")
+				var afterRange string
+				fmt.Scanln(&afterRange)
+				fmt.Println("请输入合约中加入Ido的函数名称(默认是joinIdo):")
+				var joinIdoFuncName string
+				fmt.Scanln(&joinIdoFuncName)
+
+				fmt.Println("请输入joinIdo的金额(单位是ether)")
+				var joinIdoAmount string
+				fmt.Scanln(&joinIdoAmount)
+
+				fmt.Println("ido合约是否采用白名单,如果是回复 1:")
+				var whiteFileName string
+				fmt.Scanln(&whiteFileName)
+				whiteListJoinIdoCmd.Run(cmd, []string{preRange, afterRange, mintContractAddress, abiFileName, joinIdoFuncName, joinIdoAmount, whiteFileName})
 
 			default:
 				fmt.Println("无效选择，请重新选择")
@@ -262,6 +290,48 @@ var setNetNodeCmd = &cobra.Command{
 	},
 }
 
+var whiteListJoinIdoCmd = &cobra.Command{
+	Use:   "whiteListJoinIdo",
+	Short: "批量 joinIdo",
+	Run: func(cmd *cobra.Command, args []string) {
+		if args[0] == "" || args[1] == "" {
+			log.Println("错误:: 钱包编号和金额请不要为空")
+			return
+		}
+		if args[2] == "" || args[3] == "" {
+			log.Println("错误:: 合约地址和abi文件名请不要为空")
+			return
+		}
+
+		if args[4] == "" {
+			args[4] = "joinIdo"
+		}
+
+		pre, err := strconv.Atoi(args[0])
+		after, err := strconv.Atoi(args[1])
+		if err != nil || pre < 1 || pre > 500 || after < pre || after < 1 || after > 500 {
+			log.Println("错误:: 钱包编号请为 1-500的区间里,且前置范围不能大于后置范围")
+			return
+		}
+		if args[5] == "" {
+			log.Println("错误:: 金额不能为空")
+			return
+		}
+		amountWei, err := util.ConvertEtherToWei(args[5])
+		if err != nil {
+			log.Println("错误:: 转换金额出错")
+			return
+		}
+
+		if args[6] == "1" {
+			whiteListJoinIdo.ListJoinIdoWithWithWhiteList(MemoryCache, pre, after, args[2], args[3], args[4], amountWei)
+		} else {
+			whiteListJoinIdo.JoinIdoNoWhite(MemoryCache, pre, after, args[2], args[3], args[4], amountWei)
+		}
+
+	},
+}
+
 func main() {
 	MemoryCache = cache.New(10*time.Minute, 10*time.Minute)
 	MemoryCache.Set("netNode", "http://localhost:8545", cache.NoExpiration)
@@ -271,6 +341,7 @@ func main() {
 	rootCmd.AddCommand(setMainWalletCmd)
 	rootCmd.AddCommand(getWalletKeyCmd)
 	rootCmd.AddCommand(setNetNodeCmd)
+	rootCmd.AddCommand(whiteListJoinIdoCmd)
 	// 捕获 Ctrl+C 信号
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
